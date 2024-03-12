be4178eb60e1811bfc75b3909f89bc4c37b36726
Malong-yyds <161089691@qq.com>
2024-03-12 12:22:04 +0800

    feat: 增加用户注册功能

--- src/views/login/components/LoginCard.vue
+++ src/views/login/components/LoginCard.vue
@@ -1,4 +1,3 @@
-
 <template>
   <el-card class="login-card h-60 w-96 md:rounded-lg" shadow="never">
 
@@ -6,25 +5,34 @@
       <h3>粤史之旅推荐系统</h3>
     </div>
     <div class="main">
-      <el-form ref="formRef" :model="loginForm" label-width="0">
-  
+      <el-form ref="formRef" :model="formData" label-width="0">
+
         <el-form-item class="mt-2">
-          <el-input v-model="loginForm.username" placeholder="请输入用户名">
+          <el-input v-model="formData.username" placeholder="请输入用户名">
             <template #prepend>
               <span class="w-11 text-center inline-block">用户名</span>
             </template>
           </el-input></el-form-item>
 
-        <el-form-item  class="mt-2">
-          <el-input v-model="loginForm.password" placeholder="请输入密码" type="password">
+        <el-form-item class="mt-2">
+          <el-input v-model="formData.password" placeholder="请输入密码" type="password">
             <template #prepend>
               <span class="w-11 text-center inline-block">密码</span></template>
           </el-input>
         </el-form-item>
+        <el-form-item v-if="isRegister" class="mt-2">
+          <el-input v-model="confirmPassword" placeholder="请再次输入密码" type="password">
+            <template #prepend>
+              <span class="w-11 text-center inline-block">确认密码</span></template>
+          </el-input>
+        </el-form-item>
         <el-form-item class="buttons">
-          <el-button type="primary" class="w-full left-button" size="default" @click="handleLogin">登录</el-button>
-          <el-button type="primary" class="w-full right-button" size="default" @click="handleRegister">注册</el-button>
-        
+          <el-button v-if="!isRegister" type="primary" class="w-full left-button" size="default"
+            @click="handleLogin">登录</el-button>
+          <el-button v-if="!isRegister" type="primary" class="w-full right-button" size="default"
+            @click="goRegister">注新用户册</el-button>
+          <el-button v-if="isRegister" type="primary" class="w-full right-button" size="default"
+            @click="handleRegister">确定</el-button>
         </el-form-item>
 
       </el-form>
@@ -34,73 +42,100 @@
 </template>
 <script setup lang="ts">
 
-import { login } from '/@/api/index'
+import { login, register } from '/@/api/index'
 import { useStore } from '/@/store/modules/user';
 
 const store = useStore();
-const loginForm = reactive({
+const formData = reactive({
   username: '',
   password: '',
 })
-const router=useRouter()
+const confirmPassword = ref('')
+const router = useRouter()
+const isRegister = ref(false)
 const handleLogin = async () => {
 
- await login(loginForm).then(res => {
+  await login(formData).then(res => {
 
     const token = res.data.token;
 
-    store.loginSuccess(token,loginForm.username,res.data.id);
-     // 显示登录成功的消息提示  
-     ElMessage ({  
-      message: '登录成功',  
-      type: 'success',  
-      duration: 2000, // 消息显示的时长，单位毫秒  
-      showClose: true, // 是否显示关闭按钮  
-      
-    });  
-  
-    // 跳转到首页  
-    router.push('/');  
+    store.loginSuccess(token, formData.username, res.data.id);
+    // 显示登录成功的消息提示  
+    ElMessage({
+      message: '登录成功',
+      type: 'success',
+      duration: 2000,
+      showClose: true, 
+    });
+
+    router.push('/');
   }).catch(e => {
-    // 登录失败时也可以显示消息提示  
-    ElMessage({  
-      message: '登录失败，请检查用户名和密码',  
-      type: 'error',  
-      duration: 2000,  
-      showClose: true,  
-      
-    });  
+  
+    ElMessage({
+      message: '登录失败，请检查用户名和密码',
+      type: 'error',
+      duration: 2000,
+      showClose: true,
+
+    });
     console.log(e);
   })
 }
 
-const handleRegister=()=>{
-  
+const goRegister = () => {
+  isRegister.value = true
+}
+
+const handleRegister = () => {
+  if (formData.password !== confirmPassword.value) {
+    ElMessage({
+      message: '注册失败，两次密码不一致，请重新输入',
+      type: 'error',
+      duration: 2000,
+      showClose: true,
+
+    });
+    confirmPassword.value = ''
+  }
+  else {
+    register(formData).then(res => {
+      console.log(res);
+      ElMessage({
+        message: '注册成功,跳转登录',
+        type: 'success',
+        duration: 2000,  
+        showClose: true, 
+      });
+      isRegister.value = false
+    }).catch(e => {
+      console.log(e);
+
+    })
+  }
 }
 </script>
 <style scoped>
-.card-header {  
-  margin: 0px 0px 20px 0px; /* 调整为20px以增大头部和内容之间的间距 */  
-  font-size: 36px;  
-  font-weight: bold;  
-  text-align: center;  
-}  
-  
-.main {  
-  padding: 30px; /* 设置内容部分的内边距 */  
-  display: flex; /* 启用弹性布局 */  
-  flex-direction: column; /* 子元素垂直排列 */  
-}  
-  
-.buttons{  
-  display: flex; /* 使得按钮水平排列 */  
-  justify-content: space-between; /* 按钮之间有空隙，且分别靠左右 */  
-}  
-  
-.left-button, .right-button {  
-  flex: 1; /* 使按钮平均分配可用空间 */  
-} 
-.el-input.w-60 {
-  /* width: 60px; */
+.card-header {
+  margin: 0px 0px 20px 0px;
+  font-size: 36px;
+  font-weight: bold;
+  text-align: center;
 }
+
+.main {
+  padding: 30px;
+  display: flex;
+  flex-direction: column;
+}
+
+.buttons {
+  display: flex;
+  justify-content: space-between;
+}
+
+.left-button,
+.right-button {
+  flex: 1;
+}
+
 </style>
