619d1f763aa48f0d97d85088da08087a2d9931dd
Malong-yyds <161089691@qq.com>
2024-03-21 21:28:13 +0800

    feat: 景点详情增加交通指南字段

--- src/views/scenery/SpotDetail.vue
+++ src/views/scenery/SpotDetail.vue
@@ -5,12 +5,8 @@
 
             <div class="video-overlay" @mousedown="startDrag" @mousemove="drag" @mouseup="endDrag" @mouseleave="endDrag"
                 ref="draggableDiv" v-if="spot.video">
-               
-                <video class="draggable-video" controls :src="baseUrl+spot.video" alt="景点视频"></video>
-                <!-- <iframe src="//player.bilibili.com/player.html?aid=1851189799&cid=1450750881&page=1"   
-        scrolling="no" border="0" frameborder="no" framespacing="0"   
-        allowfullscreen="true" width="640" height="480">   
-</iframe> -->
+
+                <video class="draggable-video" controls :src="baseUrl + spot.video" alt="景点视频"></video>
             </div>
             <div class="carousel">
                 <el-carousel>
@@ -26,6 +22,7 @@
                 <p><b>地址:</b> {{ spot.address }}</p>
                 <p><b>开放时间:</b> {{ spot.opening_hours ? spot.opening_hours : '具体以现场为准' }}</p>
                 <p><b>官方电话: </b>{{ spot.official_phone ? spot.official_phone : '暂无' }}</p>
+                <!-- <p><b>交通方式: </b>{{ spot.official_phone ? spot.official_phone : '暂无' }}</p> -->
 
             </div>
         </div>
@@ -35,6 +32,10 @@
             <div class="section" id="part2">
                 <h3>景点介绍</h3>
                 <p>{{ spot.description }}</p>
+            </div>
+            <div class="section" id="part8" v-if="spot.transport">
+                <h3>交通指南</h3>
+                <p>{{ spot.transport }}</p>
             </div>
             <div class="section" v-if="spot.tips" id="part3">
                 <h3>必看贴士</h3>
@@ -50,18 +51,42 @@
             <el-drawer v-model="drawer" direction="btt" size="46%">
                 <PostReview :msg="attId"></PostReview>
             </el-drawer>
-            <div class="section" id="part5">
-                <h3>周围美食</h3>
-                <p>{{ spot.description }}</p>
-            </div>
-            <div class="section" id="part6">
-                <h3>民宿酒店</h3>
-                <p>{{ spot.description }}</p>
-            </div>
             <div class="section" id="part7">
                 <h3>周边景点</h3>
                 <p>{{ spot.description }}</p>
             </div>
+            <div class="section" id="part5">
+                <h3>附近美食</h3>
+                <div class="stay">
+                    <el-card style="max-width: 200px" v-for="item in 4">
+
+                        <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+                            style="width: 100%" />
+                        <template #footer>
+                            <span>user</span>
+                            <p>这个非常好吃
+                            </p>
+                        </template>
+                    </el-card>
+                </div>
+            </div>
+            <!-- <div class="section" id="part6">
+                <h3>民宿酒店</h3>
+               
+                <div class="stay">
+                    <el-card style="max-width: 200px" v-for="item in 4">
+
+                        <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+                            style="width: 100%" />
+                        <template #footer>
+                            <span>user</span>
+                            <p>这个非常好吃
+                            </p>
+                        </template>
+                    </el-card>
+                </div>
+            </div> -->
+          
         </div>
         <el-backtop :visibility-height=0 :bottom="100" style="left: 250px">
             <el-anchor>
@@ -71,11 +96,13 @@
                     </el-icon>
                 </el-anchor-link>
                 <el-anchor-link href="#part2" title="介绍" />
+                <el-anchor-link href="#part8" title="交通" />
                 <el-anchor-link href="#part3" title="贴士" />
                 <el-anchor-link href="#part4" title="评论" />
-                <el-anchor-link href="#part5" title="美食" />
-                <el-anchor-link href="#part6" title="住宿" />
+               
+                <!-- <el-anchor-link href="#part6" title="住宿" /> -->
                 <el-anchor-link href="#part7" title="周边" />
+                <el-anchor-link href="#part5" title="美食" />
             </el-anchor>
         </el-backtop>
     </div>
@@ -89,7 +116,7 @@ import { Edit, Picture as IconPicture, Top, ArrowUpBold } from '@element-plus/ic
 import UserReview from "./components/UserReview.vue";
 import { useStore } from '/@/store/modules/user';
 import './SpotDetail.css';
-import {baseUrl} from '/@/service'
+import { baseUrl } from '/@/service'
 // 拖动参数
 const draggableDiv = ref<HTMLElement | null>(null);
 let dragging = false;
@@ -101,43 +128,43 @@ let offsetX = 0;
 let offsetY = 0;
 
 // 开始拖拽的函数  
-const startDrag = (event: MouseEvent) => {  
+const startDrag = (event: MouseEvent) => {
     // 设置dragging为true，表示拖拽操作开始  
-    dragging = true;  
+    dragging = true;
     // 计算鼠标的初始X坐标与offsetX的差值，保存到initialX  
-    initialX = event.clientX - offsetX;  
-    initialY = event.clientY - offsetY;  
-};  
-  
+    initialX = event.clientX - offsetX;
+    initialY = event.clientY - offsetY;
+};
+
 // 拖拽中的函数  
-const drag = (event: MouseEvent) => {   
-    if (dragging) {  
+const drag = (event: MouseEvent) => {
+    if (dragging) {
         // 阻止事件的默认行为  
-        event.preventDefault();  
+        event.preventDefault();
         // 计算鼠标的当前X坐标与initialX的差值，得到新的X坐标偏移量  
-        const newX = event.clientX - initialX;  
-        const newY = event.clientY - initialY;  
+        const newX = event.clientX - initialX;
+        const newY = event.clientY - initialY;
         // 更新offsetX的值  
-        offsetX = newX;  
-        offsetY = newY;  
+        offsetX = newX;
+        offsetY = newY;
         // 调用setTranslate函数，根据新的X和Y偏移量来移动元素  
-        setTranslate(newX, newY);  
-    }  
-};  
-  
+        setTranslate(newX, newY);
+    }
+};
+
 // 结束拖拽的函数  
-const endDrag = () => {  
+const endDrag = () => {
     // 设置dragging为false，表示拖拽操作结束  
-    dragging = false;  
-};  
-  
+    dragging = false;
+};
+
 // 设置元素平移的函数  
-const setTranslate = (xPos: number, yPos: number) => {  
+const setTranslate = (xPos: number, yPos: number) => {
     // 如果draggableDiv有值（即存在对应的DOM元素）  
-    if (draggableDiv.value) {  
+    if (draggableDiv.value) {
         // 使用CSS的transform属性，设置元素的平移效果  
-        draggableDiv.value.style.transform = `translate(${xPos}px, ${yPos}px)`;  
-    }  
+        draggableDiv.value.style.transform = `translate(${xPos}px, ${yPos}px)`;
+    }
 };
 
 const router = useRouter()
@@ -159,7 +186,8 @@ const showDrawer = () => {
 }
 const spot = reactive({
     name: null,
-    video:null,
+    transport:null,
+    video: null,
     image_link: '',
     address: null,
     average_score: null,
@@ -214,4 +242,3 @@ const getMapUrl = (name: string) => {
     --el-anchor-marker-bg-color: none;
 }
 </style>
-
